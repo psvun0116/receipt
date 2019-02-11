c0ffc0e40576412ded204400fd722c1afc674536
Paula Vun <paulavun@Paulas-MacBook-Pro.local>
2019-02-10 17:17:56 -0800

    created webpage receipt.html to include styles in css, bootstrap, jquery, and scripts in javascript. webpage is to fill out name, address and date to present a receipt and shipping label of purchase.

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var nameInput = $("input#name").val();
+    var addressInput= $("input#address").val();
+    var locationInput = $("input#location").val();
+    var dateInput = $("input#date").val();
+
+    $(".name").text(nameInput);
+    $(".address").text(addressInput);
+    $(".location").text(locationInput);
+    $(".date").text(dateInput);
+
+    $("#story").show();
+
+    event.preventDefault();
+
+  });
+});
